5068da4d448bfc2fdfe58d57b1c886297aa9d160
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-11-13 16:41:00 +0900

    set blurPlaceholder

--- src/fe/pages/index.tsx
+++ src/fe/pages/index.tsx
@@ -2,8 +2,50 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  const urls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 5, 7, 8, 2, 4, 9, 6].map((u) => `/${u}.jpg`)
+
+  const images = await Promise.all(
+    urls.map(async (url) => {
+      const { base64, img } = await getPlaiceholder(url);
+
+      return {
+        ...img,
+        alt: "image",
+        width: "100",
+        height: "400",
+        unoptimized: true,
+        key: url,
+        objectFit: "cover",
+        placeholder: "blur",
+        blurDataURL: base64,
+      }
+    })
+  ).then((values) => values);
+  
+  return {
+    props: {
+        images,
+    }
+  }
+}
+
+const Item = ({ ...props }) => {
+  return <Image {...props} />
+}
+
+const Items = ({ images }) => {
+  console.log(images)
+  return (
+    <div>
+      {images.map((image) => <Item { ...image } />)}
+    </div>
+  )
+}
+
+const Home: NextPage = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,55 +58,12 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Items { ...props } />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
