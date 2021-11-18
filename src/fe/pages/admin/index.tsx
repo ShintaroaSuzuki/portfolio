6a16884f89e4a403d3dc96a721404157f6f93b1b
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2021-11-19 00:46:07 +0900

    enable to use mutation in apollo client

--- src/fe/pages/admin/index.tsx
+++ src/fe/pages/admin/index.tsx
@@ -6,6 +6,16 @@ import { useState } from 'react'
 import { useAddPostMutation, usePostsQuery } from '../../generated/generated'
 import { initialValue } from '../../utils/initialText'
 import { Descendant } from 'slate'
+import { gql, useMutation } from '@apollo/client'
+
+const ADD_POST = gql`
+  mutation createPost($title: String!, $body: String!) {
+    createPost(createPostInput: {title: $title, body: $body}) {
+      title
+      body
+    }
+  }
+`
 
 const LeftIcon = () => {
   return <span className="material-icons">post_add</span>
@@ -15,14 +25,16 @@ const Admin: NextPage = (props) => {
   const [title, setTitle] = useState<string>('')
   const [body, setBody] = useState<Descendant[]>(initialValue)
   // const { loading, data }  = usePostsQuery()
-  const [ addPost, { loading, error, data } ] = useAddPostMutation()
+  const [ createPost, { loading, error, data } ] = useAddPostMutation()
+  // const [ createPost, { loading, error, data }] = useMutation(ADD_POST)
 
   if (loading) return <p>Loading ...</p>
+
   return (
     <Box maxW="960px" mx="auto">
       <Title value ={title} setValue={setTitle} />
       <RichTextEditor value={body} setValue={setBody} />
-      <Button leftIcon={<LeftIcon />} colorScheme="teal" variant="solid" onClick={() => addPost({title: 'test', body: 'test'})}>Create</Button>
+      <Button leftIcon={<LeftIcon />} colorScheme="teal" variant="solid" onClick={() => createPost({ variables: { title: 'test', body: 'test' }})}>Create</Button>
     </Box>
   )
 }
